48abb368f16f218bc6d1b537a50329726bffb0bf
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-19 16:23:46 +0530

    first commit

--- frontend/src/pages/Createpost.jsx
+++ frontend/src/pages/Createpost.jsx
@@ -19,7 +19,7 @@ const Createpost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:3000/api/v1/dalle/', {
+        const response = await fetch('https://dalle-2-0-silk.vercel.app/api/v1/dalle/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -47,7 +47,7 @@ const Createpost = () => {
     if (form.prompt && form.photo) {
       try {
         setLoading(true);
-        const response = await fetch('http://localhost:3000/api/v1/posts', { 
+        const response = await fetch('https://dalle-2-0-silk.vercel.app/api/v1/posts', { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
