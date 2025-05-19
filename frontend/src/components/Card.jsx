5ad4074f6f0ee83a9e435b6c2920353736850d5c
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-19 17:20:43 +0530

    ready commit

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({data}) => {
   <img src={data.photo} className='w-full hover:opacity-[0.4]   peer' alt="" />
   <div className='w-full h-full absolute top-0 flex flex-col flex-wrap  peer-hover:bg-[#000000d4] peer-hover:rounded-xl peer-hover:opacity-[1] opacity-0 pointer-events-none p-3'>
     <h1 className='text-[#ffff] text-2xl uppercase font-bold'>Developed by <span className='text-[#2346e2] '>{data.name}</span></h1>
-    <p className='text-xs ml-3 line-clamp-10 leading-4 mt-2 text-[#fafafd]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, temporibus dignissimos natus consequatur rem nulla, commodi minima animi eaque sint odio, excepturi culpa minus suscipit quam laborum eos. Animi, magni!</p>
+    <p className='text-xs ml-3 line-clamp-10 leading-4 mt-2 text-[#fafafd]'>{data.prompt}</p>
   </div>
 </div>
   )
